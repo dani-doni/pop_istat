e6d612b6bf05c95976e787378cfc794e23f16350
dani-doni <128325494+dani-doni@users.noreply.github.com>
2023-07-12 18:11:54 +0200

    Update pop_istat.py

--- pop_istat.py
+++ pop_istat.py
@@ -24,3 +24,17 @@ TIPO_INDDEM = "JAN"
 api_url = "https://sdmx.istat.it/SDMXWS/rest/data/22_289/{}.{}.{}.{}.{}.{}/"
 url = api_url.format (FREQ,ETA,ITTER107,SESSO,STACIVX,TIPO_INDDEM)
 st.write(url)
+
+async def main():
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+
+            print("Status:", response.status)
+            print("Content-type:", response.headers['content-type'])
+
+            html = await response.text()
+            print("Body:", html[:15], "...")
+
+
+asyncio.run(main())
