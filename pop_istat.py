6e77a7e05c6484ab1f2a8e6c665d676139b21020
dani-doni <128325494+dani-doni@users.noreply.github.com>
2023-07-12 18:10:48 +0200

    Update pop_istat.py

--- pop_istat.py
+++ pop_istat.py
@@ -3,4 +3,24 @@ import pandas as pd
 import aiohttp
 import asyncio
 
-st.write("Hello world!")
+url_CL_ITTER107 = 'https://raw.githubusercontent.com/ondata/guida-api-istat/master/processing/DCIS_POPRES1/3_Dimension_CL_ITTER107.csv'
+CL_ITTER107 = pd.read_csv(url_CL_ITTER107)
+# take data of ITTER107 and read it as csv
+
+with st.form("my_form"):
+    chosen_ITTER107 = st.selectbox('Comune', options=list(CL_ITTER107['dimensionValueDescription']))
+    id_ITTER107 = CL_ITTER107.loc[CL_ITTER107 ['dimensionValueDescription'] == chosen_ITTER107,'dimensionValueID'].iloc[0]
+    submitted = st.form_submit_button("Submit")
+#choose a ITTER107 from streamlit frontend and take the ID
+
+FREQ = "A"
+ETA = "TOTAL"
+ITTER107 = id_ITTER107
+SESSO = 9
+STACIVX = 99
+TIPO_INDDEM = "JAN"
+#declare API varibles
+
+api_url = "https://sdmx.istat.it/SDMXWS/rest/data/22_289/{}.{}.{}.{}.{}.{}/"
+url = api_url.format (FREQ,ETA,ITTER107,SESSO,STACIVX,TIPO_INDDEM)
+st.write(url)
