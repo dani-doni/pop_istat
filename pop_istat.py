f131157bdd40d3084a11f9bca8cd31be6d7b28f3
dani-doni <128325494+dani-doni@users.noreply.github.com>
2023-07-12 18:19:51 +0200

    Update pop_istat.py

--- pop_istat.py
+++ pop_istat.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
-import aiohttp
-import asyncio
+import requests
+import urllib3
+import ssl
 
 url_CL_ITTER107 = 'https://raw.githubusercontent.com/ondata/guida-api-istat/master/processing/DCIS_POPRES1/3_Dimension_CL_ITTER107.csv'
 CL_ITTER107 = pd.read_csv(url_CL_ITTER107)
@@ -25,16 +26,25 @@ api_url = "https://sdmx.istat.it/SDMXWS/rest/data/22_289/{}.{}.{}.{}.{}.{}/"
 url = api_url.format (FREQ,ETA,ITTER107,SESSO,STACIVX,TIPO_INDDEM)
 st.write(url)
 
-async def main():
+class CustomHttpAdapter (requests.adapters.HTTPAdapter):
+    # "Transport adapter" that allows us to use custom ssl_context.
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
+    def __init__(self, ssl_context=None, **kwargs):
+        self.ssl_context = ssl_context
+        super().__init__(**kwargs)
 
-            print("Status:", response.status)
-            print("Content-type:", response.headers['content-type'])
+    def init_poolmanager(self, connections, maxsize, block=False):
+        self.poolmanager = urllib3.poolmanager.PoolManager(
+            num_pools=connections, maxsize=maxsize,
+            block=block, ssl_context=self.ssl_context)
 
-            html = await response.text()
-            print("Body:", html[:15], "...")
 
+def get_legacy_session():
+    ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+    ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+    session = requests.session()
+    session.mount('https://', CustomHttpAdapter(ctx))
+    return session
+Then use it in place of the requests call:
 
-asyncio.run(main())
+get_legacy_session().get(url)
